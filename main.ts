160dc91cd279a4797cafce9ea48b874f383eb0b8
Jeritt Williams <49690937+jeritt@users.noreply.github.com>
2023-04-24 18:36:47 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
 let strip = neopixel.create(DigitalPin.P8, 5, NeoPixelMode.RGB)
 basic.forever(function () {
-    strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+    strip.setPixelColor(4, neopixel.colors(NeoPixelColors.Purple))
+    strip.show()
 })
